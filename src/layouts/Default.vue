e138b434ca6f6c494685f67f1f9494a34498bc0b
Yashu Mittal <mittalyashu77@gmail.com>
2018-10-25 18:59:40 +0530

    Use siteName as heading (#2)

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,7 +2,7 @@
   <div class="layout">
     <header class="header">
       <g-link class="heading-link" :to="{ name: 'home' }">
-        <component :is="titleTag" class="heading">Gridsome Starter Blog</component>
+        <component :is="titleTag" class="heading">{{ config.siteName }}</component>
       </g-link>
     </header>
     <slot/>
@@ -10,10 +10,15 @@
 </template>
 
 <script>
+import config from '~/.temp/config.js';
+
 export default {
   computed: {
     titleTag () {
       return this.$route.name === 'home' ? 'h1' : 'h6'
+    },
+    config () {
+      return config
     }
   }
 }
