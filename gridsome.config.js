25bb541e567bce66c5258d9c6792c439d2cd8026
Tommy Vedvik <tommyvedvik@gmail.com>
2019-02-19 20:30:15 +0100

    Add inline docs

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,6 +4,27 @@ module.exports = {
 
   plugins: [
     {
+      // Add Post collection
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/*.md',
+        typeName: 'Post',
+        route: '/:slug',
+        refs: {
+          // Add a reference to Author GraphQL collection
+          author: 'Author', 
+          
+          // Auto create a collection for all categories
+          categories: { 
+            typeName: 'Category',
+            route: '/category/:slug',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      // Add Authors collection
       use: '@gridsome/source-filesystem',
       options: {
         path: 'authors/*.md',
@@ -11,21 +32,5 @@ module.exports = {
         route: '/author/:id'
       }
     },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'blog/*.md',
-        typeName: 'Post',
-        route: '/:slug',
-        refs: {
-          author: 'Author',
-          categories: {
-            typeName: 'Category',
-            route: '/category/:slug',
-            create: true
-          }
-        }
-      }
-    }
   ]
 }
